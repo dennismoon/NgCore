031fc141789c9a3e113c9aa9661f789cb7405500
dennismoon <dmoon396@hotmail.com>
2017-02-22 20:55:50 -0600

    Several changes

--- NgCore/src/app/app.module.ts
+++ NgCore/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -17,6 +17,7 @@ import { AppComponent } from './app.component';
     HttpModule,
     AlertModule.forRoot()
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
